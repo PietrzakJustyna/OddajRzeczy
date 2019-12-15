a533d0a4789e494706fcccebe7f6d33ddf0117c2
Justyna Pietrzak <tynk713@gmail.com>
2019-12-15 16:46:21 +0100

    added anchors and working links in the app

--- oddaj_project/urls.py
+++ oddaj_project/urls.py
@@ -19,10 +19,10 @@ from django.urls import path
 from oddaj_app.views import LandingPage, AddDonation, Login, Register
 
 urlpatterns = [
-    path('', LandingPage.as_view()),
+    path('', LandingPage.as_view(), name="landing_page"),
     path('admin/', admin.site.urls),
-    path('add_donation', AddDonation.as_view()),
-    path('login', Login.as_view()),
-    path('register', Register.as_view()),
+    path('add_donation', AddDonation.as_view(), name="add_donation"),
+    path('login', Login.as_view(), name="login"),
+    path('register', Register.as_view(), name='register'),
 
 ]
